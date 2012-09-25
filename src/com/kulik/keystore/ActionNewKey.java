26b83153a34747bbb15f977cd78c8b1719c7e9c3
Yevgen Kulik <kxamed@gmail.com>
2012-09-25 08:59:31 +0300

    new

--- src/com/kulik/keystore/ActionNewKey.java
+++ src/com/kulik/keystore/ActionNewKey.java
@@ -4,6 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,18 +20,34 @@ import android.content.IntentFilter;
  */
 public class ActionNewKey extends MyAction {
 
+    private static final String TAG = ActionNewKey.class.getSimpleName();
+
+    private WeakReference<Activity> mActivity;
+
     public ActionNewKey() {
         super(new IntentFilter(ActionNewKey.class.getName()));
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        KeyItem keyItem = intent.getParcelableExtra(KeyItem.class.getName());
 
+        FragmentManager fragmentManager =  ((FragmentActivity) mActivity.get()).getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.edit_fragment);
+        if (fragment == null) {
+            Log.d(TAG, "Fragmnt is absent");
+            fragment = new EditFragmentDialog(keyItem);
+        } else {
+            Log.d(TAG, "Fragment already exist");
+            ((EditFragmentDialog) fragment).setKeyItem(keyItem);
+        }
+        ((EditFragmentDialog) fragment).show(fragmentManager, "asdfsd");
     }
 
     @Override
     public void onRegistrate(Activity activity) {
-
+       mActivity = new WeakReference<Activity>(activity)
+       ;
     }
 
     @Override
